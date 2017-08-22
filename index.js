31d0725eae965a6d2dd2b709e7e9acda4abc0fe9
Alex Pearson <apearson@five-startech.com>
2017-08-21 22:42:52 -0500

    Add milligram for basic styles

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 var Elm = require('./Main');
 var hljs = require('highlight.js');
 
-require('highlight.js/styles/github-gist.css')
-require('./main.css')
+require('milligram');
+require('highlight.js/styles/github-gist.css');
+require('./main.css');
 
 window.hljs = hljs;
 
