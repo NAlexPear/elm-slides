9de26d3fe4453f377a6298fe8d62ed208af8416f
Alex Pearson <alex@alexpear.com>
2017-09-05 18:27:39 -0500

    Reorganize environment setup into services

--- scripts/init.sh
+++
@@ -1,8 +0,0 @@
-#bin/bash
-
-if [ ! -d "./db" ]; then
-  mkdir db;
-  touch db.json;
-
-  echo '{"decks":[{"slides":[{"content":"# First Slide\\nType `e` to edit this slide"}],"title":"First Slide Deck","id":1}]}' >> ./db/db.json;
-fi
