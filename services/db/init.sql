475276c92cb29b07f8c03b710bba6075190deb97
Alex Pearson <alex@alexpear.com>
2018-01-07 17:37:15 -0600

    Set up foreign key constraints instead of json blobs

--- services/db/init.sql
+++ services/db/init.sql
@@ -2,12 +2,24 @@ CREATE SCHEMA api;
 
 CREATE TABLE api.decks (
   id serial primary key,
-  title text not null,
-  slides json
+  title text not null
 );
 
-INSERT INTO api.decks (title, slides) values
-  ('Slide Deck the First', '[{"content":"# First Deck"}]'), ('Slide Deck the Second', '[{"content":"# Second Deck"}]');
+CREATE TABLE api.slides (
+  id serial primary key,
+  deck_id integer REFERENCES api.decks ON DELETE CASCADE,
+  content text
+);
+
+INSERT INTO api.decks (title) VALUES
+  ('First Deck'),
+  ('Second Deck');
+
+INSERT INTO api.slides (deck_id, content) VALUES
+  (1, '# First Deck'),
+  (2, '# Second Deck');
 
 GRANT usage ON SCHEMA api TO web_anon;
 GRANT SELECT ON api.decks TO web_anon;
+GRANT SELECT ON api.slides TO web_anon;
+
