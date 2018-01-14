b6bd9b1776f07330523f17cf94d85ef5eab18891
Alex Pearson <alex@alexpear.com>
2018-01-13 20:38:29 -0600

    Add nested slides to deck routes

--- src/api/src/main.rs
+++ src/api/src/main.rs
@@ -14,10 +14,23 @@ extern crate serde_derive;
 use postgres::{Connection, TlsMode};
 use rocket_contrib::{Json, Value};
 
+#[derive(Serialize,Deserialize)]
+struct Slide {
+    id: i32,
+    content: String,
+}
+
 #[derive(Serialize, Deserialize)]
 struct Deck {
     id: i32,
     title: String,
+}
+
+#[derive(Serialize, Deserialize)]
+struct Presentation {
+    id: i32,
+    title: String,
+    slides: Option<Vec<Slide>>,
 }
 
 const DB_CONNECTION: &'static str = "postgres://web_anon:mysecretpassword@localhost:5432/app_db";
@@ -41,17 +54,28 @@ fn decks() -> Json<Vec<Deck>> {
 }
 
 #[get("/decks/<id>")]
-fn deck(id:i32) -> Json<Deck> {
+fn deck(id:i32) -> Json<Presentation> {
     let conn = Connection::connect(DB_CONNECTION, TlsMode::None).unwrap();
-    let rows = &conn.query("SELECT * FROM api.decks WHERE id = $1", &[&id]).unwrap();
-    let row = rows.into_iter().next().unwrap();
+    let deck_rows = &conn.query("SELECT * FROM api.decks WHERE id = $1", &[&id]).unwrap();
+    let deck_row = deck_rows.into_iter().next().unwrap();
+    let mut slides = Vec::new();
 
-    let deck = Deck {
-        id: row.get(0),
-        title: row.get(1),
+    for slide_row in &conn.query("SELECT id, content FROM api.slides WHERE deck_id = $1", &[&id]).unwrap() {
+        let slide = Slide {
+            id: slide_row.get(0),
+            content: slide_row.get(1),
+        };
+
+        slides.push(slide);
+    }
+
+    let presentation = Presentation {
+        id: deck_row.get(0),
+        title: deck_row.get(1),
+        slides: Some(slides),
     };
 
-    Json(deck)
+    Json(presentation)
 }
 
 #[error(404)]
